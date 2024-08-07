469f8ff74259bc9b64781006dca234bfacaecca3
monkey <jorgepablo48@gmail.com>
2024-08-07 12:42:37 -0600

    Se realizo un commit de la forma alterna.

--- promedio.java
+++ promedio.java
@@ -14,6 +14,8 @@ public class promedio {
             int Nota3= scanner.nextInt();
             System.out.println("Ingrese la Cuarta Nota4:");
             int Nota4= scanner.nextInt();
+        /**Se realiza la operacion de calculo realizando primero una suma de notas y luego una division dentro de la cantidad de notas
+         ingresadas para saber el promedio solicitado * */ 
             int Promedio= (Nota1+Nota2+Nota3+Nota4)/4;
             System.out.println("Su promedio es: "+ Promedio);
     }
