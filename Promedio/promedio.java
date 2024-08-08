1f0bd9a9b49d766597a885ba3a6d806b8be32a8c
monkey <jorgepablo48@gmail.com>
2024-08-08 05:32:45 -0600

    Comit inicial

---
+++ Promedio/promedio.java
@@ -0,0 +1,23 @@
+/**
+ * @author {Monkey;} 
+ * */
+import java.util.Scanner;
+public class promedio {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        //Se utiliza el comando "sout" para imprimir en pantalla el requerimiento solicitado para el usuario// 
+            System.out.println("Ingrese la Primera Nota1:");
+            int Nota1= scanner.nextInt();
+            System.out.println("Ingrese la segunda Nota2:");
+            int Nota2= scanner.nextInt();
+            System.out.println("Ingrese la Tercera Nota3:");
+            int Nota3= scanner.nextInt();
+            System.out.println("Ingrese la Cuarta Nota4:");
+            int Nota4= scanner.nextInt();
+        /**Se realiza la operacion de calculo realizando primero una suma de notas y luego una division dentro de la cantidad de notas
+         ingresadas para saber el promedio solicitado * */ 
+            int Promedio= (Nota1+Nota2+Nota3+Nota4)/4;
+            System.out.println("Su promedio es: "+ Promedio);
+    }
+
+}
